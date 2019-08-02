8b9334ea3052d50ec5d4bd3a1d65865f6b398973
eduardsberzins98 <51288639+eduardsberzins98@users.noreply.github.com>
2019-08-02 15:25:59 +0100

    Update ffmpegWriter.m

--- AcquistionScripts/ffmpegWriter.m
+++ AcquistionScripts/ffmpegWriter.m
@@ -1,3 +1,6 @@
+%FFMPEG Time Stamp file writer function
+%Take in the timestamps generated int the AV synchroniser script and writes
+%txt file specific for FFMPEG's needs
 function tsfilename=ffmpegWriter(FrameDurationSamples, sampr, fps, seconds, timenow, AVfolder)
 format long;
 TotalFrames = fps*seconds;
